9a6133133ba4876787b2f2255f5b2afc85f27a85
Harshith M <mharshith200@gmail.com>
2026-03-15 09:43:08 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-amber-400 text-2xl text-black text-center p-4 font-bold">React App</h1>
+      <p>This is a simple React application.</p>
+    </>
+  )
+}
+
+export default App
